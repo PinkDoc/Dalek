5b0e5230023dfe1d4ad09d8ebd0a9eb9b3de3fc4
ping xu <3213748560@qq.com>
2022-02-22 01:23:47 +0800

    json conf

--- Dalek.cc
+++ Dalek.cc
@@ -2,8 +2,36 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
+#include "base/jsonxx.h"
 #include "http/httpd.h"
 #include "reactor/EventLoop.h"
+
+void usage() {
+  printf(
+      "Before you start! You should move Dalek to root floder, and create a "
+      "conf.json!\n");
+  printf("conf.json should is just like: \"worker : [number] \" \n");
+  printf("and the port of Dalek: \"port: [port]\" \n");
+  printf("./Dalek");
+}
+
+void version() {
+  printf("Dalek version: 1.0.0\n");
+  printf("Author: Pink\n");
+  printf("Github: github.com/PinkDoc\n");
+}
+
+struct Conf {
+  explicit Conf(const jsonxx::Object& o) {
+    // assert((int)o.has<jsonxx::Number>("worker"));
+    //  assert((int)o.has<jsonxx::Number("port")>);
+    worker = o.get<jsonxx::Number>("worker");
+    port = o.get<jsonxx::Number>("port");
+  }
+
+  int worker;
+  int port;
+};
 
 // Kill workers and master
 static void Exterminate(int signo) {
@@ -22,26 +50,50 @@ static void SavePid() {
   fclose(to_save);
 }
 
-int main(int argc, char* argv[]) {
-  pinkx::SyncLogger::init("Dalek.log");
-
-  if (argc < 2) {
-    fprintf(stderr, "Dalek need more arguments...\n");
+jsonxx::Object InitConf() {
+  char buffer[1024];
+  FILE* file = fopen("conf.json", "r");
+  if (!file) {
+    fprintf(stderr, "Dalek: Dalek can't find conf.json\n");
+    fprintf(stderr, "Dalek: use ./Dalek --usage");
     exit(1);
   }
+  struct stat file_stat;
+  stat("conf.json", &file_stat);
+  int ret = fread(static_cast<void*>(buffer), 1024, file_stat.st_size, file);
+  if (ret < 0) {
+    fprintf(stderr, "Dalek: Dalek can't read from conf.json\n");
+  }
+  std::string buf(buffer);
+  jsonxx::Object obj;
+  obj.parse(buf);
+  return obj;
+}
+
+int main(int argc, char* argv[]) {
+  if (argc > 1) {
+    std::string_view arg(argv[1]);
+
+    if (arg == "--usage") {
+      usage();
+    }
+
+    if (arg == "--version") {
+      version();
+    }
+
+    exit(1);
+  }
+
+  pinkx::SyncLogger::init("Dalek.log");
+  jsonxx::Object o(InitConf());
+  Conf conf(o);
 
   signal(SIGPIPE, SIG_IGN);  // Client closed
   signal(SIGINT, Exterminate);
 
-  std::string_view arg(argv[1]);
-
-  if (arg != "start") {
-    fprintf(stderr, "Dalek get error argument!\n");
-    exit(1);
-  }
-
-  int port = atoi(argv[2]);
-  int numberOfWorker = atoi(argv[3]);
+  int port = conf.port;
+  int numberOfWorker = conf.worker;
 
   SavePid();
 
